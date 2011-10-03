3c7551b65a4e23d2c5761618c8f002ee617a2f36
Kristian Rosenvold <kristian.rosenvold@gmail.com>
2011-10-03 12:47:50 +0200

    Disabled logging

--- src/main/java/org/rosenvold/spring/convention/beanclassresolvers/GenericBeanClassResolver.java
+++ src/main/java/org/rosenvold/spring/convention/beanclassresolvers/GenericBeanClassResolver.java
@@ -22,15 +22,12 @@ public class GenericBeanClassResolver implements NameToClassResolver {
 
     public Class resolveBean(String name) {
         final Class aClass = resolveClass(name);
-        if ("com.telenor.consumer.stub.util.util.test.TestLoginHelper".equals(name)){
-            System.out.println("Hey");
-        }
         if (aClass != null && aClass.isInterface()) {
             final int mapperCount = mappers.length;
             for (int i = 0; i < mapperCount; i++){
                 final String beanClass = mappers[i].getBeanClassName(aClass);
                 if (beanClass != null) {
-                    logger.log(Level.INFO, "Found bean " + beanClass);
+                    //logger.log(Level.INFO, "Found bean " + beanClass);
                     final Class prospect = resolveClass(beanClass);
                     if (prospect != null && candidateEvaluator.isBean(prospect)) return prospect;
                 }
@@ -38,7 +35,7 @@ public class GenericBeanClassResolver implements NameToClassResolver {
             logger.log(Level.INFO, "No resolution for " + name);
             return null;
         }
-        logger.log(Level.INFO, "Class resolution for " + name);
+        //logger.log(Level.INFO, "Class resolution for " + name);
         return aClass != null && candidateEvaluator.isBean(aClass) ? aClass : null ;
     }
 
